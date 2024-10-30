b77554dd812dc8ff66b03e59d64eda4930e4d375
Lucille Amanda Jureidini <145380670+cdblast@users.noreply.github.com>
2024-10-30 11:45:00 -0700

    Focus 1.2.5
    To readme: List of assignments to work on
    To 1.2.5:
    Pseudocode, initializing turtle and rand module added.

--- CSP/1_2/1_2_5/a125_main_LJ.py
+++ CSP/1_2/1_2_5/a125_main_LJ.py
@@ -1,10 +1,23 @@
 # a125_main_LJ.py
+'''
+  OK so the idea is
+  -cute art/sprites (last)
+  -background (nice to have)
+
+  petals randomized (hopefully we can set origin of each sprite to the center and make rotation dependent on randomized petal count)
+  create flower
+
+  what is the name?
+  loves me, loves me not.. each time petal is plucked (using space bar i think)s
+'''
 #-----import statements-----
 import turtle as trtl
 import random as rand
 
 #-----game configuration----
-
+'''
+  maybe create petal_timesclicked to help w/ even/odd decider
+'''
 #-----initialize turtle-----
 rands = rand.Random()
 
@@ -14,7 +27,22 @@ wn = trtl.Screen()
 wn.bgcolor("white smoke")
 
 #-----game functions--------
+'''
+def make_petals():
+  sprites loaded in
+  maybe do something like spawn in, rotate(divided by random petal amount decided) and go forward if spawning at origin fucks me in the ass lol.
 
+def startgame():
+  user input "Who's your crush?"
+  make flower() (this is gonna be just the flower head lol.)
+  make petals() (function should have random generated and the petals should be either clicked or spacebar to pop off.)
+
+def petalclicked():
+  text on screen switch from "they love me, they love me not" (modulus operator my behated lol)
+
+def IM GONNA KILL MYSELF IM GONNA KILL MYSELF IM GONNA KILL MYSEL
+
+'''
 #-----events----------------
 
 #startgame()
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # APCSP
  holding all of my AP CSP homework yippee
+
+ assignments due:
+ 1.2.1 (finished, please show teacher)
+ 1.2.3
+ 1.2.4
+ 1.2.5 <--WORTH 100 POINTS WORK ON FIRST
+    ^ concept: thinking of a "they love me, they love me not" pulling petals game. small, easy. make the petal amount random, make the art cute, make the text chill lol.
+ 2.1.1
